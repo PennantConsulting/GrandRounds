fab77f04a5ded9b927ed8beec8850dfb1c8ab14a
bhwilliamson <bhwilliamson1@gmail.com>
2018-11-07 21:43:34 -0500

    updated startup script

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -it -v ${PWD}/../../src:/usr/src/app -v /usr/src/app/node_modules -p 4200:4200 --rm grand-rounds
+cd ../..
+docker run -it -v ${PWD}/../../GrandRounds:/usr/src/app -v /usr/src/app/node_modules -p 4200:4200 --rm grand-rounds
